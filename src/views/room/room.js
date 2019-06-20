8953b3550bd4628160dd6299cc1d72a8d6c21938
castro <castro@localhost.localdomain>
2019-06-20 10:15:17 -0300

    resolvendo problemas de compatibilidade dos navegadores

--- src/views/room/room.js
+++ src/views/room/room.js
@@ -8,6 +8,7 @@ const Room = ({ props }) => {
     const videoRef = useRef(null);
     const videoRefAnother = useRef(null);
     const [id, setId] = useState(null);
+    navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 
     const peer =  new Peer({
         host: '0.0.0.0',
@@ -19,35 +20,47 @@ const Room = ({ props }) => {
         var conn = peer.connect(props.match.params.id);
         conn.on('open', function(){
           conn.send('Message from that id');
-          navigator.mediaDevices.getUserMedia({video: true, audio: true})
-          .then((stream) => {
+          navigator.getUserMedia({video: true, audio: true}, function(stream) {
             const call = peer.call(props.match.params.id, stream)
             call.on('stream', function(remotestream){
-                videoRefAnother.current.src = window.URL.createObjectURL(remotestream)
+                try {
+                    videoRefAnother.current.srcObject = remotestream
+                } catch (error) {
+                    videoRefAnother.current.src = window.URL.createObjectURL(remotestream)
+                }
             })
-            videoRef.current.src = window.URL.createObjectURL(stream);
-            videoRef.current.onloadedmetadata = function(e) {
-              //fetchAB(dataStream)
-            };
+            try {
+                videoRef.current.srcObject = stream;
+            } catch (error) {
+                videoRef.current.src = (window.URL || window.webkitURL).createObjectURL(stream);
+            }
+          }, (error) => {
+              console.log(error)
           })
-          .catch((error) => console.log(error))
         });
     }
 
     peer.on('call', function(call) {
-        navigator.mediaDevices.getUserMedia({video: true, audio: true})
-        .then((stream) => {
-          call.answer(stream);
-          call.on('stream', function(remotestream){
-                videoRefAnother.current.src = window.URL.createObjectURL(remotestream)
-          })
-          const dataStream = window.URL.createObjectURL(stream);
-          videoRef.current.src = dataStream;
-          videoRef.current.onloadedmetadata = function(e) {
-            //fetchAB(dataStream)
-          };
+        navigator.getUserMedia({video: true, audio: true}, function(stream) {
+            call.answer(stream);
+            call.on('stream', function(remotestream){
+                try {
+                    videoRefAnother.current.srcObject = remotestream
+                } catch (error) {
+                    videoRefAnother.current.src = window.URL.createObjectURL(remotestream)
+                }
+            })
+
+            
+            //const dataStream = window.URL.createObjectURL(stream);
+            try {
+                videoRef.current.srcObject = stream;
+            } catch (error) {
+                videoRef.current.src = (window.URL || window.webkitURL).createObjectURL(stream);
+            }
+        }, (error) => {
+            console.log(error)
         })
-        .catch((error) => console.log(error))
     })
 
 
